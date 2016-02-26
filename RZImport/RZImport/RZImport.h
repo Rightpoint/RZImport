42a69e25e86573b227fad10182568802c4e14fc3
Derek Ostrander <djostran@gmail.com>
2016-02-25 16:21:11 -0800

    Carthage Support - Put RZImport in .xcodeproj - Add shared scheme

---
+++ RZImport/RZImport/RZImport.h
@@ -0,0 +1,19 @@
+//
+//  RZImport.h
+//  RZImport
+//
+//  Created by Derek Ostrander on 2/25/16.
+//  Copyright © 2016 Derek Ostrander. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RZImport.
+FOUNDATION_EXPORT double RZImportVersionNumber;
+
+//! Project version string for RZImport.
+FOUNDATION_EXPORT const unsigned char RZImportVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RZImport/PublicHeader.h>
+
+
